5d8090205b93861faf9eb0320c37daf3c99f0e20
Sergei Rastrigin <sergei.rastrigin@vonage.com>
2023-02-09 13:06:08 +0000

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/sms/json',
+        destination: '/api/sms',
+      },
+      {
+        source: '/_/health',
+        destination: '/api/health',
+      }
+    ]
+  },
 }
 
 module.exports = nextConfig
